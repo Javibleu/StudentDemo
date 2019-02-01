dec0462e910265c5b3ed16fff936cfea1000e52d
Javier <44002569+Javibleu@users.noreply.github.com>
2019-02-01 15:32:54 -0500

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -6,7 +6,7 @@ public class Student {
   private String studentID;
   private String name;
   private String program;
-  private String a;
+  private String course;
 
   public Student(String givenID, String givenName)
   {
